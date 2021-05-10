fe73247115d423ad1ac6aa6b65dc70595ac29cf6
alexellevu <newnapster83@gmail.com>
2021-05-10 16:01:00 +0200

    snack 1

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+//Snack 1:
+//Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+
+var zucchina = [
+    zucchina1={varietà: 'nera di milano', peso: 80, lunghezza:10},
+    zucchina2={varietà: 'alberello', peso: 50, lunghezza:8},
+    zucchina3={varietà: 'tonda di nizza', peso: 70, lunghezza:9},
+    zucchina4={varietà: 'fiorentina', peso: 100, lunghezza:12},
+    zucchina5={varietà: 'golden', peso: 62, lunghezza:15},
+    zucchina6={varietà: 'alberello', peso: 50, lunghezza:8},
+    zucchina7={varietà: 'trombetta', peso: 90, lunghezza:7},
+    zucchina8={varietà: 'faenza', peso:95 , lunghezza:14},
+    zucchina9={varietà: 'siciliana', peso: 110, lunghezza:10},
+    zucchina10={varietà: 'napoli', peso: 100, lunghezza:13}            
+];
